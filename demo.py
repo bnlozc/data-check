08421eab1407d8b3071e26f9695e13b7300a36d3
Benal <benal.clk@gmail.com>
2026-04-26 15:27:03 +0300

    Solve circle area challenge

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
-
-
+import math
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    if radius > 0:
+        return radius * radius* math.pi
+    return 0
